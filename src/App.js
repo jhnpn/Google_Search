8fbf325cfe6f3a42c8f749d09648060b4c505e09
umaiibo <j.hnpn123@gmail.com>
2020-07-19 22:14:27 -0700

    set up app as well as changed some of the styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Pages/Home/Home';
+import Search from './Pages/Search/Search';
+import Saved from './Pages/Saved/Saved';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+
+        <Router>
+          <switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/saved" component={Saved} />
+          </switch>
+        </Router>
+
+        <Footer />
+      </React.Fragment>
+
+    )
+  }
 }
 
-export default App;
+export default App
